c4a4e2597ee5e9d1cd3d182666fb41e4ba1a190d
snowwhitedev <snowwhite91924@gmail.com>
2022-03-28 22:03:47 +0700

    feat: added skip param in query

--- src/apollo/queries.js
+++ src/apollo/queries.js
@@ -4,7 +4,7 @@ export const PAGE_ITEMS = 1000;
 
 export const STAKE_POSITION = gql`
     query snapshots($skip: Int!, $pool: Int!, $balanceThreshold: Int!) {
-      stakePositions(first: 1000, skip: 0, where: {pool: $pool, stakeBalance_gte: $balanceThreshold}, orderBy: stakeBalance, orderDirection: desc) {
+      stakePositions(first: 1000, skip: $skip, where: {pool: $pool, stakeBalance_gte: $balanceThreshold}, orderBy: stakeBalance, orderDirection: desc) {
         id
         pool
         staker
